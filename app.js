561139d1f83a8416c8d36fa84b49f2682a313380
robinsonur <102710895+robinsonur@users.noreply.github.com>
2024-03-16 12:09:18 -0400

    Update app.js
    Signed-off-by: robinsonur <102710895+robinsonur@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 const port = process.env.PORT ?? 3000;
-console.log(123, 'prueba')
-app.use(express.static('public'))
+
+app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-    res.redirect('/');
-})
+
+    res.redirect('/')
+
+});
+
+wss.on('connection', ws => {
+
+    ws.on('message', message => {
+
+        console.log(`Recibido: ${message}`)
+
+    });
+
+    ws.send('Hola')
+
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+
+    console.log(`App listening on port ${port}`)
+
 })
