059039b776db0cd4ca46c5ceb9f74e52794b5741
robinsonur <102710895+robinsonur@users.noreply.github.com>
2024-03-16 12:01:34 -0400

    Update app.js
    Signed-off-by: robinsonur <102710895+robinsonur@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
-
+console.log(123, 'prueba')
 app.use(express.static('public'))
 
 app.get('*', (req, res) => {
